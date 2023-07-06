b6a9594c7e9dd4623aeba42ac2f4afa6b3779676
אודיה סעדון <osadoun@g.jct.ac.il>
2023-07-06 13:33:24 +0300

    add sql files

---
+++ SQLFiles/drop_tables.sql
@@ -0,0 +1,12 @@
+use makeacake;
+
+DROP TABLE IF EXISTS order_event;
+DROP TABLE IF EXISTS order_product;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS media_event;
+DROP TABLE IF EXISTS media_product;
+DROP TABLE IF EXISTS events_management;
+DROP TABLE IF EXISTS passwords;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS addresses;
+DROP TABLE IF EXISTS products;
