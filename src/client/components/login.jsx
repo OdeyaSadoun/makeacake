be44ae3df964327350577b8f4c2c233a36a7aeaa
אודיה סעדון <osadoun@g.jct.ac.il>
2023-07-09 22:39:54 +0300

    add componnents to client - login

---
+++ src/client/components/login.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement your login logic here
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Username/Email"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
