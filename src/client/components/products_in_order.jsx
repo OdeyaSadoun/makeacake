c977d5efd32d6337e353fb00ce4e7111f8795d19
אודיה סעדון <osadoun@g.jct.ac.il>
2023-07-09 22:40:21 +0300

    add componnents to client  - for products

---
+++ src/client/components/products_in_order.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+const ProductsInOrders = ({ orderId }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch products for the given order ID from the server
+    // Update the 'products' state with the fetched data
+  }, [orderId]);
+
+  return (
+    <div>
+      {products.map((product) => (
+        <div key={product.id}>
+          <p>Name: {product.name}</p>
+          <p>Quantity: {product.quantity}</p>
+          <p>Price: {product.price}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductsInOrders;
