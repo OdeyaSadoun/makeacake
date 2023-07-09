59b6364f97669e82e99676a5cd93388e1b7bf82e
אודיה סעדון <osadoun@g.jct.ac.il>
2023-07-09 22:41:30 +0300

    add componnents to client - toolbar

---
+++ src/client/components/toolbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Toolbar = () => {
+  return (
+    <div>
+      <button>Home</button>
+      <button>Shop</button>
+      <button>Cart</button>
+      <button>Liked Products</button>
+      <button>User Management</button>
+    </div>
+  );
+};
+
+export default Toolbar;
