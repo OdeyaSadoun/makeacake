10e5c87a9e543b117f479c0573490c1f999c3b1c
אודיה סעדון <osadoun@g.jct.ac.il>
2023-07-10 12:37:16 +0300

    fix in login - for now

--- src/client/components/login.jsx
+++ src/client/components/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-// import RestAPI from '../server/restApi';
+// import RestAPI from '../server/RestAPI';
 // import './css/Login.css';
 
 const Login = () => {
