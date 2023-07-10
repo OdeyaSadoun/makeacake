1cab04d8bc06dbd1dda8366a71f281536ee40df1
אודיה סעדון <osadoun@g.jct.ac.il>
2023-07-10 10:29:07 +0300

    the main page correct

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
+import {Route, Routes, useNavigate } from 'react-router-dom';
+import Login from './client/components/Login';
+import Register from './client/components/Register';
 import Toolbar from './client/components/Toolbar';
 import Home from './client/pages/Home';
 import Shop from './client/pages/Shop';
 import Cart from './client/pages/Cart';
 import LikedProductsPage from './client/pages/LikedProductsPage';
-import Login from './client/pages/LoginPage';
-import Register from './client/pages/RegisterPage';
 import AdminPage from './client/pages/AdminPage';
 
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+    navigate('/');
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+    navigate('/login');
+  };
+
   return (
-    <Router>
+    
       <div>
-        <Toolbar />
+        <h1>MAKEACAKE</h1>
         <Routes>
-          <Route path="/" exact component={Home} />
-          <Route path="/shop" component={Shop} />
-          <Route path="/cart" component={Cart} />
-          <Route path="/liked-products" component={LikedProductsPage} />
-          <Route path="/admin" component={AdminPage} />
-          <Route path="/register" component={Register} /> 
-          <Route path="/login" component={Login} /> {/* Add this line */}
-
-
+          <Route path="/login" element={<Login onLogin={handleLogin} />} />
+          <Route path="/register" element={<Register />} />
+          {loggedIn ? (
+            <>
+              <Toolbar onLogout={handleLogout} />
+              <Route path="/" element={<Home />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/liked-products" element={<LikedProductsPage />} />
+              <Route path="/admin" element={<AdminPage />} />
+            </>
+          ) : (
+            <Route path="/" element={<Login/>} />
+          )}
         </Routes>
       </div>
-    </Router>
+    
   );
 };
 
