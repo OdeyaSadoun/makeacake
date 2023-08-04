6b5e7e94af5b60d433ed7dc139bd622c9fff42bc
אודיה סעדון <osadoun@g.jct.ac.il>
2023-08-04 15:27:23 +0300

    fix the URLs in rest api

--- src/server/models/restapi.js
+++ src/server/models/restapi.js
@@ -1,7 +1,15 @@
 const BASE_URL = "http://localhost:3001";
-///restAPI
+const USERS_BASE_URL = `${BASE_URL}/users`;
+const ORDERS_BASE_URL = `${BASE_URL}/orders`;
+const PRODUCTS_BASE_URL = `${BASE_URL}/products`;
+const ADDRESS_BASE_URL = `${BASE_URL}/addresses`;
+const EVENTS_BASE_URL = `${BASE_URL}/events-managment`;
+
+
 export class RestAPI {
+
   static async fetchData(url, options) {
+    //general function to fetch
     var response;
     if (options) {
       response = await fetch(url, options);
@@ -15,18 +23,17 @@ export class RestAPI {
   /*Users*/
   /**********************************************************/
   static async getAllUsers() {
-    const url = `${BASE_URL}/api/users`;
+    const url = USERS_BASE_URL;
     return await RestAPI.fetchData(url);
   }
 
   static async getUserById(userid) {
-    const url = `${BASE_URL}/api/users/${userid}`;
+    const url = `${USERS_BASE_URL}/${userid}`;
     return await RestAPI.fetchData(url);
   }
 
   static async getUserByUsernameAndPassword(username, password) {
-    console.log('function getUserByUsernameAndPassword',username, password);
-    const url = `${BASE_URL}/users/login`;
+    const url = `${USERS_BASE_URL}/login`;
     const mybody = { username, password };
     const options = {
       method: "POST",
@@ -40,7 +47,7 @@ export class RestAPI {
   }
 
   static async createUser(first_last_name, username, system_password, email, phone, city, street, numhouse, date_of_birth, id_card, is_admin) { 
-    const url = `${BASE_URL}/api/users`;
+    const url = `${USERS_BASE_URL}/register`;
     const body = {first_last_name, username, system_password, email, phone, city, street, numhouse, date_of_birth, id_card, is_admin };
     const options = {
       method: "POST",
@@ -55,7 +62,7 @@ export class RestAPI {
   }
 
   static async updateEmailByUserId(userid, email) {
-    const url = `${BASE_URL}/api/users/${userid}/update_email`;
+    const url = `${USERS_BASE_URL}/update_email/${userid}`;
     const body = { email };
     const options = {
       method: "PUT",
@@ -68,7 +75,7 @@ export class RestAPI {
   }
 
   static async updatePhoneByUserId(userid, phone) {
-    const url = `${BASE_URL}/api/users/${userid}/update_phone`;
+    const url = `${USERS_BASE_URL}/update_phone/${userid}`;
     const body = { phone };
     const options = {
       method: "PUT",
@@ -81,7 +88,7 @@ export class RestAPI {
   }
 
   static async updateNameByUserId(userid, first_last_name) {
-    const url = `${BASE_URL}/api/users/${userid}/update_name`;
+    const url = `${USERS_BASE_URL}/update_name/${userid}`;
     const body = { first_last_name };
     const options = {
       method: "PUT",
@@ -94,7 +101,7 @@ export class RestAPI {
   }
 
   static async updatePasswordByUserId(userid, password) {
-    const url = `${BASE_URL}/api/users/${userid}/update_password`;
+    const url = `${USERS_BASE_URL}/update_password/${userid}`;
     const body = { password };
     const options = {
       method: "PUT",
@@ -107,7 +114,7 @@ export class RestAPI {
   }
 
   static async updateIsAdminByUserId(userid, isAdmin) {
-    const url = `${BASE_URL}/api/users/${userid}/update_is_admin`;
+    const url = `${USERS_BASE_URL}/update_is_admin/${userid}`;
     const body = { isAdmin };
     const options = {
       method: "PUT",
@@ -119,170 +126,425 @@ export class RestAPI {
     return await RestAPI.fetchData(url, options);
   }
 
-  
-  /*Orders*/
-  /*********************************************************/
 
+  /* Orders */
+  /**********************************************************/
   static async getAllOrders() {
-    const url = `${BASE_URL}/api/orders`;
+    const url = `${ORDERS_BASE_URL}`;
     return await RestAPI.fetchData(url);
   }
 
-  static async getOrderById(orderId) {
-    const url = `${BASE_URL}/api/orders/${orderId}`;
+  static async getOrderById(orderid) {
+    const url = `${ORDERS_BASE_URL}/${orderid}`;
     return await RestAPI.fetchData(url);
   }
 
-  static async getOrdersByUserId(userId) {
-    const url = `${BASE_URL}/api/users/${userId}/orders`;
+  static async getOrdersByUserId(userid) {
+    const url = `${ORDERS_BASE_URL}/user/${userid}`;
     return await RestAPI.fetchData(url);
   }
 
-  static async createOrder(order) {
-    const url = `${BASE_URL}/api/orders`;
+  static async addOrder(id, user_id, payment_type, is_paid, is_shipping, order_address_id) {
+    const url = `${ORDERS_BASE_URL}/add_order`;
+    const body = { id, user_id, payment_type, is_paid, is_shipping, order_address_id };
     const options = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(order),
+      body: JSON.stringify(body),
     };
     return await RestAPI.fetchData(url, options);
   }
 
-  static async updateOrderPaymentType(orderId, paymentType) {
-    const url = `${BASE_URL}/api/orders/${orderId}/update_payment_type`;
+  static async updatePaymentType(orderid, payment_type) {
+    const url = `${ORDERS_BASE_URL}/update_payment_type/${orderid}`;
+    const body = { payment_type };
     const options = {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ payment_type: paymentType }),
+      body: JSON.stringify(body),
     };
     return await RestAPI.fetchData(url, options);
   }
 
-  static async updateOrderIsPaid(orderId, isPaid) {
-    const url = `${BASE_URL}/api/orders/${orderId}/update_is_paid`;
+  static async updateIsPaid(orderid, is_paid) {
+    const url = `${ORDERS_BASE_URL}/update_is_paid/${orderid}`;
+    const body = { is_paid };
     const options = {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ is_paid: isPaid }),
+      body: JSON.stringify(body),
     };
     return await RestAPI.fetchData(url, options);
   }
 
-  static async updateOrderIsShipping(orderId, isShipping) {
-    const url = `${BASE_URL}/api/orders/${orderId}/update_is_shipping`;
+  static async updateIsShipping(orderid, is_shipping) {
+    const url = `${ORDERS_BASE_URL}/update_is_shipping/${orderid}`;
+    const body = { is_shipping };
     const options = {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ is_shipping: isShipping }),
+      body: JSON.stringify(body),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+
+  /* Event Management */
+  /**********************************************************/
+  static async getAllEvents() {
+    const url = `${EVENTS_BASE_URL}`;
+    return await RestAPI.fetchData(url);
+  }
+
+  static async getEventById(eventid) {
+    const url = `${EVENTS_BASE_URL}/${eventid}`;
+    return await RestAPI.fetchData(url);
+  }
+
+  static async addEvent(event_date_time, is_dairy, event_type, event_address_id, discount_percentage, comments, is_arrive) {
+    const url = `${EVENTS_BASE_URL}/add_event`;
+    const body = { event_date_time, is_dairy, event_type, event_address_id, discount_percentage, comments, is_arrive };
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async updateEvent(eventid, event_data) {
+    const url = `${EVENTS_BASE_URL}/${eventid}`;
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(event_data),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async updateEventDateTime(eventid, event_date_time) {
+    const url = `${EVENTS_BASE_URL}/update_date_time/${eventid}`;
+    const body = { event_date_time };
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async updateEventDairyStatus(eventid, is_dairy) {
+    const url = `${EVENTS_BASE_URL}/update_is_dairy/${eventid}`;
+    const body = { is_dairy };
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async updateEventType(eventid, event_type) {
+    const url = `${EVENTS_BASE_URL}/update_type/${eventid}`;
+    const body = { event_type };
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async updateEventAddress(eventid, event_address_id) {
+    const url = `${EVENTS_BASE_URL}/update_address/${eventid}`;
+    const body = { event_address_id };
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async updateEventDiscount(eventid, discount_percentage) {
+    const url = `${EVENTS_BASE_URL}/update_discount/${eventid}`;
+    const body = { discount_percentage };
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async updateEventComments(eventid, comments) {
+    const url = `${EVENTS_BASE_URL}/update_comments/${eventid}`;
+    const body = { comments };
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async updateEventArrivalStatus(eventid, is_arrive) {
+    const url = `${EVENTS_BASE_URL}/update_is_arrival/${eventid}`;
+    const body = { is_arrive };
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async deleteEvent(eventid) {
+    const url = `${EVENTS_BASE_URL}/${eventid}`;
+    const options = {
+      method: "DELETE",
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+
+  /* Address Management */
+  /**********************************************************/
+  static async getAllAddresses() {
+    const url = `${ADDRESSES_BASE_URL}`;
+    return await RestAPI.fetchData(url);
+  }
+
+  static async getAddressById(addressid) {
+    const url = `${ADDRESSES_BASE_URL}/${addressid}`;
+    return await RestAPI.fetchData(url);
+  }
+
+  static async addAddress(street, city, state, postal_code, country) {
+    const url = `${ADDRESSES_BASE_URL}/add_address`;
+    const body = { street, city, state, postal_code, country };
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async updateAddress(addressid, address_data) {
+    const url = `${ADDRESSES_BASE_URL}/${addressid}`;
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(address_data),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async updateAddressStreet(addressid, street) {
+    const url = `${ADDRESSES_BASE_URL}/update_street/${addressid}`;
+    const body = { street };
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async updateAddressCity(addressid, city) {
+    const url = `${ADDRESSES_BASE_URL}/update_city/${addressid}`;
+    const body = { city };
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async updateAddressState(addressid, state) {
+    const url = `${ADDRESSES_BASE_URL}/update_state/${addressid}`;
+    const body = { state };
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async updateAddressPostalCode(addressid, postal_code) {
+    const url = `${ADDRESSES_BASE_URL}/update_postal_code/${addressid}`;
+    const body = { postal_code };
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async updateAddressCountry(addressid, country) {
+    const url = `${ADDRESSES_BASE_URL}/update_country/${addressid}`;
+    const body = { country };
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+    return await RestAPI.fetchData(url, options);
+  }
+
+  static async deleteAddress(addressid) {
+    const url = `${ADDRESSES_BASE_URL}/${addressid}`;
+    const options = {
+      method: "DELETE",
     };
     return await RestAPI.fetchData(url, options);
   }
 
   
-  /*Products*/
-  /*********************************************************/
+  /* Products */
+  /**********************************************************/
   static async getAllProducts() {
-    const url = `${BASE_URL}/api/users/:userid/products`;
+    const url = `${PRODUCTS_BASE_URL}`;
     return await RestAPI.fetchData(url);
   }
 
-  static async getProductById(productId) {
-    const url = `${BASE_URL}/api/users/:userid/products`;
+  static async getProductById(productid) {
+    const url = `${PRODUCTS_BASE_URL}/${productid}`;
     return await RestAPI.fetchData(url);
   }
 
-  static async getProductsByUserId(userId) {
-    const url = `${BASE_URL}/api/users/:userid/products`;
+  static async getAllUserProducts(userid) {
+    const url = `${PRODUCTS_BASE_URL}/user/${userid}`;
     return await RestAPI.fetchData(url);
   }
 
-  static async createProduct(product) {
-    const url = `${BASE_URL}/api/users/:userid/products`;
+  static async addProduct(productData) {
+    const url = `${PRODUCTS_BASE_URL}/add_product`;
     const options = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(product),
+      body: JSON.stringify(productData),
     };
     return await RestAPI.fetchData(url, options);
   }
 
-  static async updateProductPrice(productId, price) {
-    const url = `${BASE_URL}/api/products/:productid/update_price`;
+  static async updateProductPrice(productid, price) {
+    const url = `${PRODUCTS_BASE_URL}/update_price/${productid}`;
+    const body = { price };
     const options = {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ price }),
+      body: JSON.stringify(body),
     };
     return await RestAPI.fetchData(url, options);
   }
 
-  static async updateProductQuantity(productId, quantity) {
-    const url = `${BASE_URL}/api/products/:productid/update_quantity`;
+  static async updateProductQuantity(productid, quantity) {
+    const url = `${PRODUCTS_BASE_URL}/update_quantity/${productid}`;
+    const body = { quantity };
     const options = {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ quantity }),
+      body: JSON.stringify(body),
     };
     return await RestAPI.fetchData(url, options);
   }
 
-  static async updateProductSensitivity(productId, sensitivity) {
-    const url = `${BASE_URL}/api/products/:productid/update_sensitivity`;
+  static async updateProductSensitivity(productid, sensitivity) {
+    const url = `${PRODUCTS_BASE_URL}/update_sensitivity/${productid}`;
+    const body = { sensitivity };
     const options = {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ sensitivity }),
+      body: JSON.stringify(body),
     };
     return await RestAPI.fetchData(url, options);
   }
 
-  static async updateProductKosherType(productId, kosherType) {
-    const url = `${BASE_URL}/api/products/:productid/update_kosher`;
+  static async updateProductKosher(productid, kosher) {
+    const url = `${PRODUCTS_BASE_URL}/update_kosher/${productid}`;
+    const body = { kosher };
     const options = {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ kosher_type: kosherType }),
+      body: JSON.stringify(body),
     };
     return await RestAPI.fetchData(url, options);
   }
 
-  static async updateProductIsDairy(productId, isDairy) {
-    const url = `${BASE_URL}/api/products/:productid/update_is_dairy`;
+  static async updateProductIsDairy(productid, is_dairy) {
+    const url = `${PRODUCTS_BASE_URL}/update_is_dairy/${productid}`;
+    const body = { is_dairy };
     const options = {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ is_dairy: isDairy }),
+      body: JSON.stringify(body),
     };
     return await RestAPI.fetchData(url, options);
   }
 
-  static async deleteProduct(productId) {
-    const url = `${BASE_URL}/api/users/:userid/products`;
+  static async deleteProduct(productid) {
+    const url = `${PRODUCTS_BASE_URL}/${productid}`;
     const options = {
       method: "DELETE",
     };
     return await RestAPI.fetchData(url, options);
   }
 }
-
 export default RestAPI;
