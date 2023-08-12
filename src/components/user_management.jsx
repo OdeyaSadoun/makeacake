b483886b8667616ad51a7142a94d956c55d5305a
אודיה סעדון <osadoun@g.jct.ac.il>
2023-08-12 19:39:56 +0300

    list of all users for manager

--- src/components/user_management.jsx
+++ src/components/user_management.jsx
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import RestAPI from '../server/models/restapi';
 
-const userManagement = () => {
+const UserManagement = () => {
   const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await RestAPI.getAllUsers();
+      if (response) {
+        setUsers(response);
+      } else {
+        console.error('Failed to fetch users');
+      }
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   const handleAddUser = () => {
     // Implement logic to add a new user
@@ -20,7 +38,7 @@ const userManagement = () => {
       <button onClick={handleAddUser}>Add User</button>
       {users.map((user) => (
         <div key={user.id}>
-          <p>Name: {user.name}</p>
+          <p>Name: {user.first_last_name}</p>
           <p>Email: {user.email}</p>
           <button onClick={() => handleEditUser(user.id)}>Edit</button>
           <button onClick={() => handleDeleteUser(user.id)}>Delete</button>
@@ -30,4 +48,4 @@ const userManagement = () => {
   );
 };
 
-export default userManagement;
+export default UserManagement;
