f99b5d261aeb5b04e4cfa0efe195ba2075eafcfa
אודיה סעדון <osadoun@g.jct.ac.il>
2023-08-18 18:16:41 +0300

    fix in product - add not work

--- src/components/addProduct.jsx
+++ src/components/addProduct.jsx
@@ -17,18 +17,9 @@ const AddProduct = () => {
 
     const navigate = useNavigate();
 
-    const [product, setProduct] = useState({
-        product_name: '',
-        is_dairy: false,
-        price: 0,
-        discount_percentage: 0,
-        kosher_type: 'bet_yosef',
-        comments: '',
-        sensitivity: '',
-    });
+    const handleAddProduct = async (e) => {
+        e.preventDefault(); // Prevent default form submission
 
-
-    const handleAddProduct = async () => {
         const newProduct = await RestAPI.addProduct(
             product_name,
             is_dairy,
@@ -50,28 +41,16 @@ const AddProduct = () => {
         }
     };
 
-    const handleInputChange = (event) => {
-        const { name, value, type, checked } = event.target;
-        const inputValue = type === 'checkbox' ? checked : value;
-        setProduct((prevProduct) => ({ ...prevProduct, [name]: inputValue }));
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Send the product data to your server API using fetch or axios
-        console.log('Submitting product:', product);
-    };
-
     return (
         <div>
             <h2>הוספת מוצר חדש</h2>
-            <form onSubmit={handleSubmit}>
+            <form>
                 <div>
                     <label>שם:</label>
                     <input
                         type="text"
                         name="product_name"
-                        value={product.product_name}
+                        value={product_name}
                         required
                         onChange={(e) => setProductName(e.target.value)}
                     />
@@ -81,17 +60,17 @@ const AddProduct = () => {
                     <input
                         type="checkbox"
                         name="is_dairy"
-                        checked={product.is_dairy}
+                        checked={is_dairy}
                         required
-                        onChange={(e) => setIsDairy(e.target.value)}
-                    />
+                        onChange={(e) => setIsDairy(e.target.checked)}
+                        />
                 </div>
                 <div>
                     <label>מחיר:</label>
                     <input
                         type="number"
                         name="price"
-                        value={product.price}
+                        value={price}
                         required
                         onChange={(e) => setPrice(e.target.value)}
                     />
@@ -101,7 +80,7 @@ const AddProduct = () => {
                     <input
                         type="number"
                         name="discount_percentage"
-                        value={product.discount_percentage}
+                        value={discount_percentage}
                         onChange={(e) => setDiscountPercentage(e.target.value)}
                     />
                 </div>
@@ -109,7 +88,7 @@ const AddProduct = () => {
                     <label>סוג הכשרות:</label>
                     <select
                         name="kosher_type"
-                        value={product.kosher_type}
+                        value={kosher_type}
                         required
                         onChange={(e) => setKosherType(e.target.value)}
                     >
@@ -123,7 +102,7 @@ const AddProduct = () => {
                     <input
                         type="text"
                         name="comments"
-                        value={product.comments}
+                        value={comments}
                         onChange={(e) => setComments(e.target.value)}
                     />
                 </div>
@@ -132,7 +111,7 @@ const AddProduct = () => {
                     <input
                         type="text"
                         name="sensitivity"
-                        value={product.sensitivity}
+                        value={sensitivity}
                         onChange={(e) => setSensitivity(e.target.value)}
                     />
                 </div>
